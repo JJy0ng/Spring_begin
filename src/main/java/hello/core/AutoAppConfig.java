e2fa389f63f97e6e6a808d33286df1d3d9a64b9c
JJ0ng <2000jiyong@gmail.com>
2023-03-01 18:31:13 +0900

    basePackages

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.FilterType;
 @Configuration
 //@Component 애노테이션이 붙은 클래스를 찾아 자동으로 스프링 빈에 등록을 시켜줌, 등록될 때 앞 클래스 명 소문자로 등록된다.
 @ComponentScan(
+        basePackages = "hello.core.member", //멤버에 관한 클래스만 등록이 된다
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 ) // 기존 AppConfig를 뺌 -> 충돌 방지, @Configuration에 들어가면 @Component가 있는 것을 알 수 있음
 
