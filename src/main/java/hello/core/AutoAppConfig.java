8b74460fa9efde841f87fd3d433d668d9c9e02b2
JJ0ng <2000jiyong@gmail.com>
2023-03-03 20:14:43 +0900

    탐색 위치와 기본 스캔 대상, 필터, 중복 등록과 충돌

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -1,16 +1,25 @@
 package hello.core;
 
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
 
 @Configuration
-//@Component 애노테이션이 붙은 클래스를 찾아 자동으로 스프링 빈에 등록을 시켜줌, 등록될 때 앞 클래스 명 소문자로 등록된다.
+//@Component 애노테이션이 붙은 클래스를 찾아 자동으로 스프링 빈에 등록을 시켜줌, 등록될 때 앞 클래스 명 소문자로 등록된다
+//애노테이션에는 상속관계가 없으며 스프링이 지원하는 기능이다
 @ComponentScan(
-        basePackages = "hello.core.member", //멤버에 관한 클래스만 등록이 된다
+//        basePackages = "hello.core.member", //멤버에 관한 클래스만 등록이 된다, 탐색할 패키지의 시작 위치를 지정한다. 이를 포함해서 하위 패키지 탐색
+//        basePackageClasses = AutoAppConfig.class, //지정한 클래스의 패키지를 탐색 시작 위치로 지정
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 ) // 기존 AppConfig를 뺌 -> 충돌 방지, @Configuration에 들어가면 @Component가 있는 것을 알 수 있음
 
-public class AutoAppConfig {
-
+//자동 빈과 자동 빈 경우에는 오류가 나옴
+public class AutoAppConfig { //수동 등록 빈이 우선권을 가짐, 로그가 나옴
+    @Bean(name = "memoryMemberRepository")
+    MemberRepository memberRepository(){
+        return new MemoryMemberRepository();
+    } //빈 수동 등록
 }
--- src/main/java/hello/core/CoreApplication.java
+++ src/main/java/hello/core/CoreApplication.java
@@ -3,9 +3,10 @@ package hello.core;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication //여기에 @ComponentScan이 있다.
 public class CoreApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CoreApplication.class, args);
 	}
+	//자동 빈과 수동 빈 이름이 같을 때 여기서 오류가 나도록 한다
 }
