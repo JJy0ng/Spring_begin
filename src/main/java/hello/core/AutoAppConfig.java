4761000efa26fd68a12f96445720e9eecabb3602
JJ0ng <2000jiyong@gmail.com>
2023-03-04 22:56:41 +0900

    다양한 의존관계 주입 방법

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -1,11 +1,16 @@
 package hello.core;
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
+import hello.core.order.OrderService;
+import hello.core.order.OrderServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
+import org.springframework.core.OrderComparator;
 
 @Configuration
 //@Component 애노테이션이 붙은 클래스를 찾아 자동으로 스프링 빈에 등록을 시켜줌, 등록될 때 앞 클래스 명 소문자로 등록된다
@@ -18,6 +23,14 @@ import org.springframework.context.annotation.FilterType;
 
 //자동 빈과 자동 빈 경우에는 오류가 나옴
 public class AutoAppConfig { //수동 등록 빈이 우선권을 가짐, 로그가 나옴
+
+    //스프링 설정을 목적으로 하는 곳에서만 필드 주입 사용 가능
+    @Autowired MemberRepository memberRepository;
+    @Autowired DiscountPolicy discountPolicy;
+    @Bean
+    OrderService orderService(){
+        return new OrderServiceImpl(memberRepository, discountPolicy);
+    }
     @Bean(name = "memoryMemberRepository")
     MemberRepository memberRepository(){
         return new MemoryMemberRepository();
--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -8,11 +8,31 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class OrderServiceImpl implements OrderService{
-    private final MemberRepository memberRepository;
+    private final MemberRepository memberRepository; //앞에 @Autowired을 붙이면 필드 주입 -> 안티 패턴(사용하지 말자), 테스트 코드에 사용
     private final DiscountPolicy discountPolicy;
 
+    //수정자(setter) 주입, 필드에 final이 없어야 함, 선택, 변경 가능성이 있는 의존관계에 사용
+    /*
+    @Autowired(required = false) -> 주입할 대상이 없어도 동작가능하게 함
+    public void setMemberRepository(MemberRepository memberRepository){
+        this.memberRepository = memberRepository;
+    }
+
     @Autowired
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+    public void setDiscountPolicy(DiscountPolicy discountPolicy){
+        this.discountPolicy = discountPolicy;
+    }
+
+    @Autowired // 일반 메서드 주입, 한 번에 여러 필드를 주입 받을 수 있음 -> 일반적으로 사용하진 않음, final x
+    public void init(MemberRepository memberRepository, DiscountPolicy discountPolicy){
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
+    의존관계 자동 주입은 스프링 컨테이너가 관리하는 스프링 빈이어야 동작한다.
+     */
+
+    //@Autowired x -> 생성자가 1개 있으면 @Autowired 생략 가능 불변, 필수 의존 관계에서 사용
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) { //생성자 주입 방식
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
     }
