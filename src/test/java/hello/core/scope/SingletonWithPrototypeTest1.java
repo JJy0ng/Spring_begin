b1b5816ec686f43aadb6c6addf08314456657ed3
JJ0ng <2000jiyong@gmail.com>
2023-03-08 22:35:11 +0900

    프로토타입 스코프 - 싱글톤 빈과 함께 사용시 문제해결

--- src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
+++ src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
@@ -2,12 +2,14 @@ package hello.core.scope;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Scope;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
+import javax.inject.Provider;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -33,21 +35,16 @@ public class SingletonWithPrototypeTest1 {
 
         ClientBean clientBean2 = ac.getBean(ClientBean.class);
         int count2 = clientBean2.logic();
-        assertThat(count2).isEqualTo(2); //같은 것을 쓰는 것이기에 2가 된다
+        assertThat(count2).isEqualTo(1); //같은 것을 쓰는 것이기에 2가 된다
     }
 
-    //스프링은 일반적으로 싱글톤 빈을 사용하므로, 싱글톤 빈이 프로토타입 빈을 사용하게 된다
-    //싱글톤 빈은 생성 시점에만 의존관계 주입을 받기 때문에, 프로타입빈이 새로 생성되나 싱글톤 빈과 함께 유지되는 것이 문제점
     @Scope("singleton")
     static class ClientBean {
-        private final PrototypeBean prototypeBean; // 생성시점에 주입 -> 같은 것만 줌
-
         @Autowired
-        public ClientBean(PrototypeBean prototypeBean){
-            this.prototypeBean = prototypeBean;
-        }
+        private Provider<PrototypeBean> prototypeBeanObjectProvider;
 
         public int logic() {
+            PrototypeBean prototypeBean = prototypeBeanObjectProvider.get(); //찾아주는 기능만 제공(DL), 자바 표준
             prototypeBean.addCount();
             int count = prototypeBean.getCount();
             return count;
