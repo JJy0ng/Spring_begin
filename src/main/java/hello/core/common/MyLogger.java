a819eae97c7686328692fd9dafe7efcc212199e9
JJ0ng <2000jiyong@gmail.com>
2023-03-10 21:59:59 +0900

    request 스코프 - 문제발생

---
+++ src/main/java/hello/core/common/MyLogger.java
@@ -0,0 +1,36 @@
+package hello.core.common;
+
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import java.util.UUID;
+//로그를 출력하기 위해서 만듬
+@Component
+@Scope(value = "request") // 빈은 HTTP 요청 당 하나씩 생성, 요청이 끝나면 소멸
+public class MyLogger {
+    private String uuid;
+    private String requestURL;
+
+    //빈이 생성되는 시점에는 알 수 없으므로, 외부에서 setter로 입력 받음
+    public void setRequestURL(String requestURL) {
+        this.requestURL = requestURL;
+    }
+
+    public void log(String message){
+        System.out.println("[" + uuid + "]" + "[" + requestURL + "] " + "[" + message + "]");
+    }
+
+    @PostConstruct //초기화 메서드를 사용해서 uuid를 생성해서 저장
+    public void init(){
+        uuid = UUID.randomUUID().toString(); //절대로 아이디가 겹치지가 않음, 겹칠 확률이 극히 낮다
+        System.out.println("[" + uuid + "] request scope bean create:" + this);
+    }
+
+    @PreDestroy
+    public void close(){
+        System.out.println("[" + uuid + "] request scope bean close:" + this);
+    }
+}
---
+++ src/main/java/hello/core/web/LogDemoController.java
@@ -0,0 +1,28 @@
+package hello.core.web;
+
+import hello.core.common.MyLogger;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+@RequiredArgsConstructor
+public class LogDemoController {
+
+    private final LogDemoService logDemoService;
+    private final MyLogger myLogger;
+
+    @RequestMapping("log-demo")
+    @ResponseBody
+    public String logDemo(HttpServletRequest request){ //고객 요청 정보를 받을 수 있음
+        String requestURL = request.getRequestURL().toString(); //HTTP 요청 당 각각 구분되므로 값이 섞이지 않는다
+        myLogger.setRequestURL(requestURL);
+
+        myLogger.log(("controller test"));
+        logDemoService.logic("testId");
+        return "OK";
+    }
+}
